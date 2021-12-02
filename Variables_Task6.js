508f1b6b3d691264639f9877474d8e1cad4b15f0
Ara <ara.akopyan@gmail.com>
2021-12-02 08:13:48 +0400

    feat: read over Comparison chapter [javascript.info] , and changed exercise to practice

---
+++ Variables_Task6.js
@@ -0,0 +1,7 @@
+4 > 5 // false
+"Yandex" > "pineapple" // true
+"265" > "32" // false
+undefined == null // true because values null and undefined equal each other only.
+undefined === null // false because we have strict equality. Different types from both sides lead to false.
+null == "\n0\n" //  false
+null === +"\n0\n" // false
