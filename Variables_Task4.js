1ddb868a61485f7f94e907131669a73b380d407c
Ara <ara.akopyan@gmail.com>
2021-12-02 08:07:16 +0400

    feat: solved the task [swap the value without 3rd variable], and it takes only 3 iterations

---
+++ Variables_Task4.js
@@ -0,0 +1,9 @@
+let a = 10
+let b = 3
+// Task: need to get a = 3 , b = 10 (swap the values without third variable)
+b=a+b
+a=b-a
+b=b-a
+//First iteration: obviously b = 13 
+//Second: so now a = 10
+//Third: and now b = 13 - 10 = 3
